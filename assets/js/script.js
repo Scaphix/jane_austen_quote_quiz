147ea223efcf5187f413d408bf6a4366977e1588
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-09-01 23:00:45 +0200

    Add intro melody and mute button with icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", music);
+
+function music() {
+    let soundEnabled = false;  // global sound state
+    let muteBtn = document.getElementById("mute-btn");
+    muteBtn.addEventListener("click", function () { 
+        soundEnabled = !soundEnabled;
+   
+    // If turning sound OFF → pause melody if playing
+    if (soundEnabled===true) {
+      document.getElementById("intro-melody").play();
+        muteBtn.innerHTML = `<i class="fa-solid fa-volume-high"></i>` ;
+    } else {
+        document.getElementById("intro-melody").pause(); 
+         muteBtn.innerHTML =  `<i class="fa-solid fa-volume-xmark"></i> ` ;
+    }
+  });
+}
+
+
 // DOM Elements
 const startScreen = document.getElementById("start-screen");
 const beginScreen = document.getElementById("name-screen");
